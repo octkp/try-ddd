941fc745c2d5577953af723b9776f79527fba8e8
octkp <takano_y@kokopelli-inc.com>
2023-11-15 18:53:22 +0900

    2-4 修正

--- src/chapter2/FullName.php
+++ src/chapter2/FullName.php
@@ -1,6 +1,10 @@
 <?php
 namespace TakanoY\TryDdd\Chapter2;
 
+/**
+ * 2-4 氏名を表現するFullNameクラス
+ */
+
 final readonly class FullName
 {
 	public function __construct(
@@ -18,11 +22,17 @@ final readonly class FullName
 		return $this->lastName;
 	}
 	
+	/**
+	 * 日本での氏名
+	 */
 	public function getFullNameInJapan(): string
 	{
 		return $this->lastName . ' ' . $this->firstName;
 	}
 	
+	/**
+	 * 海外での氏名
+	 */
 	public function getFullNameInGlobal(): string
 	{
 		return $this->firstName . ' ' . $this->lastName;
